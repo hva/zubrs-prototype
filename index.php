d183a174bfe443962d607768607b5b21fc51b98c
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-02-04 22:45:36 +0100

    twig init

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'lib/twig/lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
+echo $twig->render('index.html');
+
