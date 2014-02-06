ef781ba4ee3d6df03568fc6354c7144d563d526f
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-02-06 13:28:37 +0100

    composer support

--- _init.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once 'lib/Twig/Autoloader.php';
-Twig_Autoloader::register();
-$loader = new Twig_Loader_Filesystem('templates');
-$twig = new Twig_Environment($loader);
--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
-require '_init.php';
+require 'vendor/autoload.php';
+
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader);
 
 echo $twig->render('index.html');
