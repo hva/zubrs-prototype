fa2708fc87a20c7287549955c1091751cb2b5e75
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-02-12 18:30:49 +0100

    analytics added

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 require 'vendor/Twig/Autoloader.php';
 
 Twig_Autoloader::register();
+
+require 'twig_ext.php';
+
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader);
+$twig->addExtension(new Bz_Twig_Extension());
 
 echo $twig->render('index.html');
