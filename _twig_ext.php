8857ba8782cad397755b5ea9afb7a8f9abd28207
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-02-22 13:25:34 +0100

    menu.json

--- _twig_ext.php
+++ _twig_ext.php
@@ -16,18 +16,9 @@ class Bz_Twig_Extension extends Twig_Extension {
 	}
 
 	public function getGlobals() {
-		$server = $_SERVER['SERVER_NAME'];
-		$controller = substr($_SERVER['SCRIPT_NAME'], 1, -4);
-
 		return array(
-			'localhost' => $server === 'localhost',
-			'topMenu' => array(
-				array('url' => 'news.php', 'title' => 'новости', 'active' => ($controller === 'news')),
-				array('url' => 'teams.php', 'title' => 'команды', 'active' => ($controller === 'teams')),
-				array('url' => 'competitions.php', 'title' => 'соревнования', 'active' => ($controller === 'competitions')),
-				array('url' => 'management.php', 'title' => 'руководство', 'active' => ($controller === 'management')),
-				array('url' => 'history.php', 'title' => 'история', 'active' => ($controller === 'history')),
-				),
+			'localhost' => $_SERVER['SERVER_NAME'],
+			'script' => substr($_SERVER['SCRIPT_NAME'], 1),
 			);
 	}
 
