a86e29c9f294eb0d727c27b4e9f7b38cb350fbe8
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-03-23 15:56:48 +0100

    child news section

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php
 require '_init.php';
 
-echo $twig->render('index.html');
+$news = load_json('news');
+$main = array();
+$ch = array();
+foreach ($news as $id => $x) {
+    if (isset($x['section']) && $x['section'] === 'child') {
+        $ch[$id] = $x;
+    }
+    else if(isset($x['img'])) {
+        $main[$id] = $x;
+    }
+}
+
+echo $twig->render('index.html', array(
+    'news_main' => $main,
+    'news_child' => $ch,
+));
