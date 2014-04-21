dbc2064a54c2cf2807fb10268237ee455864b734
Yauheni Khvaliuk <hvaluk@gmail.com>
2014-04-21 20:53:49 +0200

    index - results at top

--- index.php
+++ index.php
@@ -2,18 +2,30 @@
 require '_init.php';
 
 $news = load_json('news');
-$main = array();
-$ch = array();
+$news_main = array();
+$news_ch = array();
 foreach ($news as $id => $x) {
     if (isset($x['section']) && $x['section'] === 'child') {
-        $ch[$id] = $x;
+        $news_ch[$id] = $x;
     }
     else if(isset($x['img'])) {
-        $main[$id] = $x;
+        $news_main[$id] = $x;
+    }
+}
+
+$teams = load_json('teams');
+$games = load_json('games');
+$games_visible = array();
+foreach ($games as $id => $x) {
+    $team_home = $teams[$x['team_home']];
+    $team_away = $teams[$x['team_away']];
+    if (!isset($team_home['hidden']) || !isset($team_away['hidden'])) {
+        $games_visible[$id] = $x;
     }
 }
 
 echo $twig->render('index.html', array(
-    'news_main' => $main,
-    'news_child' => $ch,
+    'news_main' => $news_main,
+    'news_child' => $news_ch,
+    'games' => $games_visible,
 ));
